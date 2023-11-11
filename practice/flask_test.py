5491c6b2837b45dd4a1e9de3e7ba212c00a64ab2
Irene Lee <86876293+juwonleedev@users.noreply.github.com>
2023-11-12 00:11:22 +0900

    sample blockchain

--- practice/flask_test.py
+++ practice/flask_test.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from datetime import datetime
 from flask import render_template
+from flask import request #서버와 데이터를 주고받는 flask 코드
 
 app = Flask(__name__)
+
+test_id = "Python"
+test_pw = "Blockchain"
 
 @app.route('/')
 def index():
@@ -12,4 +16,18 @@ def index():
 @app.route('/backend_sample')
 def backend_sample():
     return render_template('backend_sample.html', backend_result = "1000개!!")
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    if request.method=='POST':
+        print("login 버튼을 누름")
+    input_value = request.form.to_dict(flat=False)
+    print(input_value)
+    if (input_value['wallet_id'][0] == test_id) & (input_value['password'][0]==test_pw):
+        print("로그인 성공")
+        return "로그인 성공!!!"
+    else:
+        return render_template('login.html')
+    return render_template('login.html')
+
 app.run()
