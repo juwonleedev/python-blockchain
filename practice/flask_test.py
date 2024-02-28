b557bca4d7abd1e23d9735dd922b2b0423666658
Irene Lee <86876293+juwonleedev@users.noreply.github.com>
2024-02-28 17:25:57 +0900

    flask

--- practice/flask_test.py
+++ practice/flask_test.py
@@ -14,8 +14,18 @@ test_coin_count = 10000
 def index():
      return 'Flask 웹사이트다!'
 
+<<<<<<< HEAD
 @app.route('/login', methods=['GET', 'POST'])
 def login():   
+=======
+# 코드를 실행시키고 나오는 http 주소로 이동
+@app.route('/backend_sample')
+def backend_sample():
+    return render_template('backend_sample.html', backend_result = "1000개!!")
+
+@app.route('/login',methods=['GET', 'POST']) # API
+def login():
+>>>>>>> 5ef4248 (Added API comment)
     if request.method=='POST':
         print("login 버튼을 누름")
         input_value = request.form.to_dict(flat=False)
